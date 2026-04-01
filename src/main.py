171ad2ee2f7a2398521a9cd9cfaf976d3f4756e4
tayele-alt <tayele@caldwell.edu>
2026-04-01 14:03:55 +0000

    Apr 1, 2026, 10:03 AM

--- src/main.py
+++ src/main.py
@@ -73,7 +73,7 @@ class Player(pygame.sprite.Sprite):
 #Platorms
 class Platfrom(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
-        super().__init
+        super().__init__()
         self.image = pygame.Surface((w, h))
         self.image.fill(GRAY)
         self.rect = self.image.get_rect(topleft=(x,y))
