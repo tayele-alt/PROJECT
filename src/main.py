61005ec4768f99775eb3e4b82b21000a9c3a7adc
tayele-alt <tayele@caldwell.edu>
2026-04-14 03:46:28 +0000

    Apr 13, 2026, 11:46 PM

--- src/main.py
+++ src/main.py
@@ -352,8 +352,8 @@ class Game:
 
     def _draw(self):
         self.screen.fill(BLACK)
-        pygame.draw.rect(self.screen, (0, 60, 0), (0, 0, 65, SCREEN_HEIGHT))
-        pygame.draw.rect(self.screen, GREEN, (0, 0, 65, SCREEN_WIDTH), 2)
+        pygame.draw.rect(self.screen, (0, 60, 0), (0, 350, 65, 130))
+        pygame.draw.rect(self.screen, GREEN, (0, 350, 65, 130), 2)
         for i in range(0, SCREEN_WIDTH, 40):
             pygame.draw.line(self.screen, DARK_GRAY, (i, 0), (i, SCREEN_HEIGHT), 1)
         for i in range(0, SCREEN_HEIGHT, 40):
