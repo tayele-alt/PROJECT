24252fafd02012c66731f7590d87665485a51f95
tayele-alt <tayele@caldwell.edu>
2026-04-14 04:31:04 +0000

    Apr 14, 2026, 12:31 AM

--- src/main.py
+++ src/main.py
@@ -388,7 +388,8 @@ class Game:
             self.player.has_flag = False
             self.score +=1
             self.enemy.base_speed = 120 + (self.score * 15)
-            self.enemy.speed = self.enemy.base_speed + (self.score * 20)
+            self.enemy.speed = self.enemy.base_speed
+            self.enemy.level - min(self.score, 3)
             self.flag = Flag(720, 130)
             self.all_sprites.add(self.flag)
 
