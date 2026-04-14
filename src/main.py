68bb89aac840776711a938c7dd06a7d11d50f67e
tayele-alt <tayele@caldwell.edu>
2026-04-14 01:22:56 +0000

    Apr 13, 2026, 9:22 PM

--- src/main.py
+++ src/main.py
@@ -299,6 +299,8 @@ class Game:
                     self.flag = Flag(720, 130)
                     self.all_sprites.add(self.flag)
                 self.player.reset()
+                self.enemy.rect.bottomleft = (600, 480)
+                self.enemy.velocity_y = 0
 
         #Pickup the Flag
         if not self.flag.collected and self.player.rect.colliderect(self.flag.rect):
