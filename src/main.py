7b31a3a1dd6ed86e0431f247686199a2f78fdcd6
tayele-alt <tayele@caldwell.edu>
2026-04-14 04:39:53 +0000

    Apr 14, 2026, 12:39 AM

--- src/main.py
+++ src/main.py
@@ -393,6 +393,27 @@ class Game:
             self.flag = Flag(720, 130)
             self.all_sprites.add(self.flag)
 
+        # Level 3 ** - enemy shoots
+        if self.enemy.level >= 3:
+            self.enemy.shoot_timer += delta
+            if self.enemy.shoot_timer >= 2.0:
+                self.enemy.shoot_tier = 0
+                direction = 1 if self.player.rect.centerx > self.enemy.rect.centerx else -1
+                eb = EnemyBullet(self.enemy.rect.centerx, self.enemy.rect.centery, direction)
+                self.enemy_bullets.add(eb)
+                self.all_sprites.add(eb)
+
+        self.enemy_bullets.update(delta)
+
+        for eb in self.enemy_bullets:
+            if eb.rect.colliderect(self.player.rect):
+                eb.kill()
+                if self.player.has_flag:
+                    self.player.has_flag = False
+                    self.flag = Flag(720, 130)
+                    self.all_sprites.add(self.flag)
+                self,self._player_hit()
+
         #When enemy catches player
         if self.player.rect.colliderect(self.enemy.rect):
             if self.player.has_flag:
